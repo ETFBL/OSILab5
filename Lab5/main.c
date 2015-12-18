b008335711936a72fb229de45919d9fc27c83cab
ETFBL <nickyleeyy@linuxmail.org>
2015-12-18 15:32:11 +0100

    prva verzija projekta

---
+++ Lab5/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Student
+ *
+ * Created on December 18, 2015, 3:31 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("Lab 5");
+    return (EXIT_SUCCESS);
+}
+
